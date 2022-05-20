acfd07458eaa668ed6f44ceb083a00f583114328
oocheol <oocheol@naver.com>
2022-05-20 17:30:07 +0900

    22/05/20

--- app/src/main/java/com/example/myapplication0520/MainActivity.java
+++ app/src/main/java/com/example/myapplication0520/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication0520;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
         // Java의 Main 메소드!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 버튼이 클릭 되었을 때 "버튼클릭"을 출력하고 싶음
+
     }
+    public void click(View view){
+        // View : 뷰 객체를 받을꺼에요
+        // --> 뷰에 대한 정보를 받아올껍니다!
+        // Log.d --> System.out.println()
+        // Log.d("Tag 안내문구", "출력시키고 싶은 내용")
+        Log.d("출력문","버튼클릭!");
+    }
+
 }
